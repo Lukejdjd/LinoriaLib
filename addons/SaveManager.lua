7e820b7acd398dde30748c67f500ef822034a951
Luke <85708954+Lukejdjd@users.noreply.github.com>
2022-09-13 18:42:01 -0400

    Update SaveManager.lua

--- addons/SaveManager.lua
+++ addons/SaveManager.lua
@@ -1,12 +1,12 @@
-local httpService = game:GetService('HttpService')
+local httpService = game:GetService("HttpService")
 
 local SaveManager = {} do
-	SaveManager.Folder = "Luke's Hub"
+	SaveManager.Folder = "LinoriaLibSettings"
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {
 			Save = function(idx, object) 
-				return { type = 'Toggle', idx = idx, value = object.Value } 
+				return { type = "Toggle", idx = idx, value = object.Value } 
 			end,
 			Load = function(idx, data)
 				if Toggles[idx] then 
@@ -16,7 +16,7 @@ local SaveManager = {} do
 		},
 		Slider = {
 			Save = function(idx, object)
-				return { type = 'Slider', idx = idx, value = tostring(object.Value) }
+				return { type = "Slider", idx = idx, value = tostring(object.Value) }
 			end,
 			Load = function(idx, data)
 				if Options[idx] then 
@@ -26,7 +26,7 @@ local SaveManager = {} do
 		},
 		Dropdown = {
 			Save = function(idx, object)
-				return { type = 'Dropdown', idx = idx, value = object.Value, mutli = object.Multi }
+				return { type = "Dropdown", idx = idx, value = object.Value, mutli = object.Multi }
 			end,
 			Load = function(idx, data)
 				if Options[idx] then 
@@ -36,7 +36,7 @@ local SaveManager = {} do
 		},
 		ColorPicker = {
 			Save = function(idx, object)
-				return { type = 'ColorPicker', idx = idx, value = object.Value:ToHex() }
+				return { type = "ColorPicker", idx = idx, value = object.Value:ToHex() }
 			end,
 			Load = function(idx, data)
 				if Options[idx] then 
@@ -46,14 +46,25 @@ local SaveManager = {} do
 		},
 		KeyPicker = {
 			Save = function(idx, object)
-				return { type = 'KeyPicker', idx = idx, mode = object.Mode, key = object.Value }
+				return { type = "KeyPicker", idx = idx, mode = object.Mode, key = object.Value }
 			end,
 			Load = function(idx, data)
 				if Options[idx] then 
 					Options[idx]:SetValue({ data.key, data.mode })
 				end
 			end,
-		}
+		},
+
+		Input = {
+			Save = function(idx, object)
+				return { type = "Input", idx = idx, text = object.Value }
+			end,
+			Load = function(idx, data)
+				if Options[idx] and type(data.text) == "string" then
+					Options[idx]:SetValue(data.text)
+				end
+			end,
+		},
 	}
 
 	function SaveManager:SetIgnoreIndexes(list)
@@ -62,8 +73,13 @@ local SaveManager = {} do
 		end
 	end
 
+	function SaveManager:SetFolder(folder)
+		self.Folder = folder;
+		self:BuildFolderTree()
+	end
+
 	function SaveManager:Save(name)
-		local fullPath = self.Folder .. '/' .. name .. '.json'
+		local fullPath = self.Folder .. "/settings/" .. name .. ".json"
 
 		local data = {
 			objects = {}
@@ -84,7 +100,7 @@ local SaveManager = {} do
 
 		local success, encoded = pcall(httpService.JSONEncode, httpService, data)
 		if not success then
-			return false, 'failed to encode data'
+			return false, "failed to encode data"
 		end
 
 		writefile(fullPath, encoded)
@@ -92,11 +108,11 @@ local SaveManager = {} do
 	end
 
 	function SaveManager:Load(name)
-		local file = self.Folder .. '/' .. name .. '.json'
-		if not isfile(file) then return false, 'invalid file' end
+		local file = self.Folder .. "/settings/" .. name .. ".json"
+		if not isfile(file) then return false, "invalid file" end
 
 		local success, decoded = pcall(httpService.JSONDecode, httpService, readfile(file))
-		if not success then return false, 'decode error' end
+		if not success then return false, "decode error" end
 
 		for _, option in next, decoded.objects do
 			if self.Parser[option.type] then
@@ -106,19 +122,147 @@ local SaveManager = {} do
 
 		return true
 	end
-    
+
 	function SaveManager:IgnoreThemeSettings()
 		self:SetIgnoreIndexes({ 
 			"BackgroundColor", "MainColor", "AccentColor", "OutlineColor", "FontColor", -- themes
-			"ThemeManager_ThemeList", 'ThemeManager_CustomThemeList', 'ThemeManager_CustomThemeName', -- themes
+			"ThemeManager_ThemeList", "ThemeManager_CustomThemeList", "ThemeManager_CustomThemeName", -- themes
 		})
+	end
+
+	function SaveManager:BuildFolderTree()
+		local paths = {
+			self.Folder,
+			self.Folder .. "/themes",
+			self.Folder .. "/settings"
+		}
+
+		for i = 1, #paths do
+			local str = paths[i]
+			if not isfolder(str) then
+				makefolder(str)
+			end
+		end
+	end
+
+	function SaveManager:RefreshConfigList()
+		local list = listfiles(self.Folder .. "/settings")
+
+		local out = {}
+		for i = 1, #list do
+			local file = list[i]
+			if file:sub(-5) == ".json" then
+				-- i hate this but it has to be done ...
+
+				local pos = file:find(".json", 1, true)
+				local start = pos
+
+				local char = file:sub(pos, pos)
+				while char ~= "/" and char ~= "\\" and char ~= "" do
+					pos = pos - 1
+					char = file:sub(pos, pos)
+				end
+
+				if char == "/" or char == "\\" then
+					table.insert(out, file:sub(pos + 1, start - 1))
+				end
+			end
+		end
+		
+		return out
 	end
 
 	function SaveManager:SetLibrary(library)
 		self.Library = library
 	end
-    
-	makefolder(SaveManager.Folder)
+
+	function SaveManager:LoadAutoloadConfig()
+		if isfile(self.Folder .. "/settings/autoload.txt") then
+			local name = readfile(self.Folder .. "/settings/autoload.txt")
+
+			local success, err = self:Load(name)
+			if not success then
+				return self.Library:Notify("Failed to load autoload config: " .. err)
+			end
+
+			self.Library:Notify(string.format("Auto loaded config %q", name))
+		end
+	end
+
+
+	function SaveManager:BuildConfigSection(tab)
+		assert(self.Library, "Must set SaveManager.Library")
+
+		local section = tab:AddRightGroupbox("Configuration")
+
+		section:AddDropdown("SaveManager_ConfigList", { Text = "Config list", Values = self:RefreshConfigList(), AllowNull = true })
+		section:AddInput("SaveManager_ConfigName",    { Text = "Config name" })
+
+		section:AddDivider()
+
+		section:AddButton("Create config", function()
+			local name = Options.SaveManager_ConfigName.Value
+
+			if name:gsub(" ", "") == "" then 
+				return self.Library:Notify("Invalid config name (empty)", 2)
+			end
+
+			local success, err = self:Save(name)
+			if not success then
+				return self.Library:Notify("Failed to save config: " .. err)
+			end
+
+			self.Library:Notify(string.format("Created config %q", name))
+
+			Options.SaveManager_ConfigList.Values = self:RefreshConfigList()
+			Options.SaveManager_ConfigList:SetValues()
+			Options.SaveManager_ConfigList:SetValue(nil)
+		end):AddButton("Load config", function()
+			local name = Options.SaveManager_ConfigList.Value
+
+			local success, err = self:Load(name)
+			if not success then
+				return self.Library:Notify("Failed to load config: " .. err)
+			end
+
+			self.Library:Notify(string.format("Loaded config %q", name))
+		end)
+
+		section:AddButton("Overwrite config", function()
+			local name = Options.SaveManager_ConfigList.Value
+
+			local success, err = self:Save(name)
+			if not success then
+				return self.Library:Notify("Failed to overwrite config: " .. err)
+			end
+
+			self.Library:Notify(string.format("Overwrote config %q", name))
+		end)
+		
+		section:AddButton("Autoload config", function()
+			local name = Options.SaveManager_ConfigList.Value
+			writefile(self.Folder .. "/settings/autoload.txt", name)
+			SaveManager.AutoloadLabel:SetText("Current autoload config: " .. name)
+			self.Library:Notify(string.format("Set %q to auto load", name))
+		end)
+
+		section:AddButton("Refresh config list", function()
+			Options.SaveManager_ConfigList.Values = self:RefreshConfigList()
+			Options.SaveManager_ConfigList:SetValues()
+			Options.SaveManager_ConfigList:SetValue(nil)
+		end)
+
+		SaveManager.AutoloadLabel = section:AddLabel("Current autoload config: none", true)
+
+		if isfile(self.Folder .. "/settings/autoload.txt") then
+			local name = readfile(self.Folder .. "/settings/autoload.txt")
+			SaveManager.AutoloadLabel:SetText("Current autoload config: " .. name)
+		end
+
+		SaveManager:SetIgnoreIndexes({ "SaveManager_ConfigList", "SaveManager_ConfigName" })
+	end
+
+	SaveManager:BuildFolderTree()
 end
 
 return SaveManager
